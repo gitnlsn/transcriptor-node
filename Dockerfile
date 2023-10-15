c14f2c1800776e9cd461d6edea4a7943293e1225
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2023-10-15 18:43:12 -0300

    update trancribe implementation

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY . ${DIR}
 
 # Install infra tools
 RUN apk update
-RUN apk add shadow build-base git coreutils wget curl bash ffmpeg
+RUN apk add shadow build-base git coreutils wget curl bash ffmpeg jq
 RUN apk add --update util-linux
 RUN git clone https://github.com/gitnlsn/whisper.cpp.git
 # RUN mv /usr/bin/wget /usr/bin/wget2
--- src/utils/transcribe-whisper.ts
+++ src/utils/transcribe-whisper.ts
@@ -14,33 +14,17 @@ export const transcribeWhisper = async ({
   filePath,
   modelPath,
 }: TranscribeWhisperProps) => {
-  const outputFilename = formatTranscriptionOutput({ filePath });
-
-  await execPromise(
-    `whisper-cpp -f ${filePath} -m ${modelPath} -l pt -bo 10 -p 2 -oj -of ${outputFilename}`
-  );
-
   const transcription = await execPromise(
-    `jq '.transcription | .[] | { start: .timestamps.from, end: .timestamps.to, text: .text }' ${outputFilename}.json | jq -s --compact-output`
-  )
-    .then(({ stdout }) => JSON.parse(stdout) as WhisperSpeech[])
-    .then((speechList) =>
-      speechList.map(({ start: startString, end: endString, text }) => {
-        const start = convertTimeToSeconds(startString);
-        const end = convertTimeToSeconds(endString);
-        const duration = end - start;
-
-        return {
-          time: start,
-          duration,
-          text,
-        } as Speech;
-      })
-    );
-
-  await execPromise(`rm ${outputFilename}.json`);
+    `whisper-cpp -f ${filePath} -m ${modelPath} -l pt -nt`
+  ).then(({ stdout }) => {
+    return {
+      time: 0,
+      duration: 5,
+      text: stdout.replace("\n", "").trim(),
+    };
+  });
 
   return {
-    transcription,
+    transcription: [transcription],
   };
 };
